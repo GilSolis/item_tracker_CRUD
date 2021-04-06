f0bb4a596f6f454809cd2fe8059656e1f4180af6
Gil Solis <gilsolisuw@gmail.com>
2021-04-05 22:00:40 -0700

    put and get functional

--- server.js
+++ server.js
@@ -2,16 +2,35 @@ const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
 const PORT = 9999
+require('dotenv').config()
 
 let db,
     dbConnectionStr = process.env.DB_STRING,
     dbName = 'items'
 
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+    .then(client => {
+        console.log(`Connected to ${dbName} DB`)
+        db = client.db(dbName)
+    })
 
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
+app.get('/', async (req, response) => {
+    let collectionData = await db.collection('items').find().toArray()
+    response.render('index.ejs', { data: collectionData })
+})
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
+app.post('/addItem', (request, response) => {
+    console.log(request.body)
+    db.collection('items').insertOne(request.body)
+        .then(result => {
+            console.log("Item Added")
+            response.redirect('/')
+        })
 })
 
 app.listen(PORT, () => {
